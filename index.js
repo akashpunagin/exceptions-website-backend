35cfc5c64e9a4d7de2db852d91b87caf19744b93
akashpunagin <akash.punagin@gmail.com>
2023-01-14 10:54:52 +0530

    profile:me done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 //Routers
 const authRouter = require("./routes/auth/authRouter");
+const profileRouter = require("./routes/profile/profileRouter");
 
 const PORT = process.env.PORT;
 
@@ -14,6 +15,7 @@ app.use(express.json());
 
 //Routes
 app.use("/auth", authRouter);
+app.use("/profile", profileRouter);
 
 app.get("/", (req, res) => {
   res.send("API working");
