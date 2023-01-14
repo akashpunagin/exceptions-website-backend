5db193309b5875999e73acb93355e35326c11618
akashpunagin <akash.punagin@gmail.com>
2023-01-14 10:42:19 +0530

    middleware:validate inputs: cleaned code, add logic for registering all types of users

--- middleware/validateInputs.js
+++ middleware/validateInputs.js
@@ -10,8 +10,19 @@ function isValidEmail(userEmail) {
 }
 
 function handleAuthReq(req) {
-  const { userId, email, contactNumber, firstName, lastName, password } =
-    req.body;
+  const {
+    userId,
+    email,
+    contactNumber,
+    firstName,
+    lastName,
+    password,
+    collegeName,
+    usn,
+    state,
+    city,
+    zip,
+  } = req.body;
 
   if (req.path === "/register-admin") {
     if (![email, firstName, lastName, contactNumber, password].every(Boolean)) {
@@ -23,21 +34,8 @@ function handleAuthReq(req) {
     }
   }
 
-  if (req.path === "/register-customer") {
-    if (
-      ![
-        email,
-        firstName,
-        lastName,
-        country,
-        state,
-        city,
-        zip,
-        address,
-        contact_number,
-        password,
-      ].every(Boolean)
-    ) {
+  if (req.path === "/register-coordinator") {
+    if (![email, contactNumber, firstName, lastName, password].every(Boolean)) {
       return missingCredsMessage;
     }
     if (!isValidEmail(email)) {
@@ -45,19 +43,19 @@ function handleAuthReq(req) {
     }
   }
 
-  if (req.path === "/register-tenant") {
+  if (req.path === "/register-participant") {
     if (
       ![
         email,
+        contactNumber,
         firstName,
         lastName,
-        country,
+        password,
+        collegeName,
+        usn,
         state,
         city,
         zip,
-        address,
-        contact_number,
-        password,
       ].every(Boolean)
     ) {
       return missingCredsMessage;
@@ -89,167 +87,15 @@ function handleAuthReq(req) {
   }
 }
 
-function handleDeviceReq(req) {
-  const {
-    device_id,
-    user_id,
-    client_topic,
-    variant,
-    hw_ver,
-    fw_ver,
-    o_logo,
-    o_prod_name,
-    o_prod_ver,
-    u_dev_name,
-    u_comp_name,
-    u_tz_diff,
-    u_lat,
-    u_long,
-    u_conn_ssid,
-  } = req.body;
-
-  if (req.path === "/add") {
-    if (
-      ![
-        device_id,
-        user_id,
-        client_topic,
-        variant,
-        hw_ver,
-        fw_ver,
-        o_logo,
-        o_prod_name,
-        o_prod_ver,
-        u_dev_name,
-        u_comp_name,
-        u_tz_diff,
-        u_lat,
-        u_long,
-      ].every(Boolean)
-    ) {
-      return missingCredsMessage;
-    }
-  }
-}
-
-function handleSensorValueError(req) {
-  const { deviceId, parameter } = req.body;
-
-  if (req.path === "/get-all-data") {
-    if (![deviceId].every(Boolean)) {
-      return missingCredsMessage;
-    }
-  }
-  if (req.path === "/get-data") {
-    if (![deviceId, parameter].every(Boolean)) {
-      return missingCredsMessage;
-    }
-  }
-}
-
-function handleTenetReq(req) {
-  const { customerId } = req.body;
-
-  if (req.path === "/get") {
-    if (![customerId].every(Boolean)) {
-      return missingCredsMessage;
-    }
-  }
-}
-
-function handleCustomerReq(req) {
-  const { tenantId, deviceId } = req.body;
-
-  if (req.path === "/add") {
-    if (![tenantId, deviceId].every(Boolean)) {
-      return missingCredsMessage;
-    }
-  }
-  if (req.path === "/delete") {
-    if (![tenantId, deviceId].every(Boolean)) {
-      return missingCredsMessage;
-    }
-  }
-}
-
-function handleSensorMasterReq(req) {
-  const {
-    device_id,
-    sensor_idx,
-    sensor_name,
-    sensor_uom,
-    sensor_report_group,
-  } = req.body;
-
-  if (req.path === "/add") {
-    if (
-      ![
-        device_id,
-        sensor_idx,
-        sensor_name,
-        sensor_uom,
-        sensor_report_group,
-      ].every(Boolean)
-    ) {
-      return missingCredsMessage;
-    }
-  }
-
-  if (req.path === "/get-sensors-of-device") {
-    if (![device_id].every(Boolean)) {
-      return missingCredsMessage;
-    }
-  }
-
-  if (req.path === "/delete") {
-    if (
-      ![
-        device_id,
-        sensor_idx,
-        sensor_name,
-        sensor_uom,
-        sensor_report_group,
-      ].every(Boolean)
-    ) {
-      return missingCredsMessage;
-    }
-  }
-}
-
 module.exports = (req, res, next) => {
   const authError = handleAuthReq(req);
-  const deviceError = handleDeviceReq(req);
-  const sensorValueError = handleSensorValueError(req);
-  const tenantError = handleTenetReq(req);
-  const customerError = handleCustomerReq(req);
-  const sensorMasterError = handleSensorMasterReq(req);
 
   console.log({
     authError,
-    deviceError,
-    sensorValueError,
-    tenantError,
-    customerError,
-    sensorMasterError,
   });
 
-  if (authError) {
+  if (req.originalUrl.includes("/auth/") && authError) {
     return res.status(401).json({ error: authError });
-  }
-  if (req.originalUrl.includes("/device/") && deviceError) {
-    return res.status(401).json({ error: deviceError });
-  }
-  if (req.originalUrl.includes("/sensor/") && sensorValueError) {
-    return res.status(401).json({ error: sensorValueError });
-  }
-  if (req.originalUrl.includes("/tenant/") && tenantError) {
-    return res.status(401).json({ error: tenantError });
-  }
-  if (req.originalUrl.includes("/customer/") && customerError) {
-    return res.status(401).json({ error: customerError });
-  }
-  if (req.originalUrl.includes("/sensorMaster/") && sensorMasterError) {
-    return res.status(401).json({ error: sensorMasterError });
   }
 
   next();
