c434aedab7f99454001d1cf833b42ea8d9be8a16
akashpunagin <akash.punagin@gmail.com>
2023-01-19 10:12:03 +0530

    team:add authorize participant

--- middleware/exportMiddlewares.js
+++ middleware/exportMiddlewares.js
@@ -2,10 +2,12 @@ const authorization = require("./authorization");
 const authorizeRefreshToken = require("./authorizeRefreshToken");
 const validateInputs = require("./validateInputs");
 const authorizeAdmin = require("./authorizeAdmin");
+const authorizeParticipant = require("./authorizeParticipant");
 
 module.exports = {
   authorization,
   authorizeRefreshToken,
   validateInputs,
   authorizeAdmin,
+  authorizeParticipant,
 };
--- routes/team/teamRoutes/add.js
+++ routes/team/teamRoutes/add.js
@@ -1,6 +1,7 @@
 const pool = require("../../../db/pool");
 const {
   authorization,
+  authorizeParticipant,
   validateInputs,
 } = require("../../../middleware/exportMiddlewares");
 const appConstants = require("../../../constants/appConstants");
@@ -10,53 +11,57 @@ const {
 } = require("../../../dbUtils/team_master/dbTeamMasterUtils");
 
 module.exports = (router) => {
-  router.post("/add", [authorization, validateInputs], async (req, res) => {
-    console.log("Route:", req.originalUrl);
+  router.post(
+    "/add",
+    [authorization, authorizeParticipant, validateInputs],
+    async (req, res) => {
+      console.log("Route:", req.originalUrl);
 
-    const { teamMaster } = appConstants.SQL_TABLE;
+      const { teamMaster } = appConstants.SQL_TABLE;
 
-    try {
-      const { name, isGCConsidered } = req.body;
+      try {
+        const { name, isGCConsidered } = req.body;
 
-      const currentUser = req.user;
-      const headUserId = currentUser.userId;
+        const currentUser = req.user;
+        const headUserId = currentUser.userId;
 
-      const teamRes = await pool.query(
-        `SELECT team_id 
+        const teamRes = await pool.query(
+          `SELECT team_id 
             FROM ${teamMaster}
             WHERE team_name = $1`,
-        [name]
-      );
-      if (teamRes.rowCount > 0) {
-        return res.status(401).json({ error: "Team already exists" });
+          [name]
+        );
+        if (teamRes.rowCount > 0) {
+          return res.status(401).json({ error: "Team already exists" });
+        }
+
+        const isUserExists = await isUserExistsByUserId(headUserId);
+        if (!isUserExists) {
+          return res.status(401).json({ error: "User does not exists" });
+        }
+
+        const isTeamHeadUserExists = await isTeamHeadExists(headUserId);
+        if (isTeamHeadUserExists) {
+          return res
+            .status(401)
+            .json({ error: "User is already a team head of another team" });
+        }
+
+        // const addRes = await pool.query(
+        //   `INSERT INTO ${teamMaster}(team_name, team_head_user, team_is_gc_considered)
+        //     VALUES($1, $2, $3)
+        //     RETURNING *`,
+        //   [name, headUserId, isGCConsidered]
+        // );
+
+        return res.status(200).json({
+          status: "Team added successfully",
+          data: "addRes.rows[0]",
+        });
+      } catch (error) {
+        console.log("ADD Team error", error);
+        return res.status(500).json("Server error");
       }
-
-      const isUserExists = await isUserExistsByUserId(headUserId);
-      if (!isUserExists) {
-        return res.status(401).json({ error: "User does not exists" });
-      }
-
-      const isTeamHeadUserExists = await isTeamHeadExists(headUserId);
-      if (isTeamHeadUserExists) {
-        return res
-          .status(401)
-          .json({ error: "User is already a team head of another team" });
-      }
-
-      const addRes = await pool.query(
-        `INSERT INTO ${teamMaster}(team_name, team_head_user, team_is_gc_considered)
-          VALUES($1, $2, $3)
-          RETURNING *`,
-        [name, headUserId, isGCConsidered]
-      );
-
-      return res.status(200).json({
-        status: "Team added successfully",
-        data: addRes.rows[0],
-      });
-    } catch (error) {
-      console.log("ADD Team error", error);
-      return res.status(500).json("Server error");
     }
-  });
+  );
 };
