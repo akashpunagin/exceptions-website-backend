87ff31bd9437dce03d18e5de752b8f74186c47bd
akashpunagin <akash.punagin@gmail.com>
2023-01-19 11:17:06 +0530

    testing commit 3

--- constants/appConstants.js
+++ constants/appConstants.js
@@ -27,7 +27,7 @@ const constants = {
     eventMaster: "event_master",
     teamMaster: "team_master",
 
-    teamMemberMaster: "team_member_master", // used to store details of team members
+    teamMemberMaster: "team_member_master", //  used to store details of team members
     teamIdTeamMember: "team_id_team_member", // used to map team (team id) and team member (member id)
     teamIdTeamMemberEvent: "team_id_team_member_event", // used to map team (team id), team member (member id) and event (event id)
   },
