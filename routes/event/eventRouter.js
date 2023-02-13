999fee930553356fd5642f5b6645c1da57b9a5e7
akashpunagin <akash.punagin@gmail.com>
2023-02-13 15:22:09 +0530

    event: get open events done

--- routes/event/eventRouter.js
+++ routes/event/eventRouter.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 require("./eventRoutes/add")(router);
 require("./eventRoutes/delete")(router);
 require("./eventRoutes/get")(router);
+require("./eventRoutes/get-open-events")(router);
 require("./eventRoutes/get-team-members-by-event-id")(router);
 require("./eventRoutes/update")(router);
 
