2d8642f94decce84508f781b7e7d703990ee8ce2
akashpunagin <akash.punagin@gmail.com>
2023-02-15 11:22:49 +0530

    scripts: get long event details done

--- scripts/getLongEevntDetails.js
+++ scripts/getLongEevntDetails.js
@@ -4,18 +4,71 @@ const { eventsList } = require("../constants/eventList");
 
 const {
   eventMaster,
-  //   eventMasterRule,
-  //   eventMasterRequirement,
-  //   eventMasterContact,
+  eventMasterRule,
+  eventMasterRequirement,
+  eventMasterContact,
   eventMasterDetails,
 } = appConstants.SQL_TABLE;
 
+async function getRulesByEventId(eventId) {
+  const getRes = await pool.query(
+    `
+  SELECT 
+    ${eventMasterRule}.*
+  FROM
+    ${eventMaster},
+    ${eventMasterRule}
+  WHERE
+    ${eventMaster}.event_id = ${eventMasterRule}.event_id AND
+    ${eventMasterRule}.event_id = $1
+  `,
+    [eventId]
+  );
+
+  const data = getRes.rows;
+
+  const rules = [];
+  for (const ruleData of data) {
+    const { event_rule: rule } = ruleData;
+    rules.push(rule);
+  }
+  return rules;
+}
+
+async function getRequirementsByEventId(eventId) {
+  const getRes = await pool.query(
+    `
+  SELECT
+    ${eventMasterRequirement}.*
+  FROM
+    ${eventMaster},
+    ${eventMasterRequirement}
+  WHERE
+    ${eventMaster}.event_id = ${eventMasterRequirement}.event_id AND
+    ${eventMasterRequirement}.event_id = $1
+  `,
+    [eventId]
+  );
+
+  const data = getRes.rows;
+
+  const requirements = [];
+  for (const ruleRequirement of data) {
+    const { event_requirement: requirement } = ruleRequirement;
+    requirements.push(requirement);
+  }
+  return requirements;
+}
+
 async function main() {
   const getRes = await pool.query(`
-        SELECT * 
-        FROM ${eventMaster}, ${eventMasterDetails}
-        WHERE ${eventMaster}.event_id = ${eventMasterDetails}.event_id
-    `);
+  SELECT * 
+  FROM
+    ${eventMaster},
+    ${eventMasterContact}
+  WHERE
+    ${eventMaster}.event_id = ${eventMasterContact}.event_id
+  `);
 
   const data = getRes.rows;
 
@@ -24,26 +77,41 @@ async function main() {
     const {
       event_id: id,
       event_name: name,
-      event_to: to,
       event_description: description,
       event_img: img,
-      event_direction: direction,
-      event_color: color,
-      event_text: text,
-      event_type: eventType,
+
+      event_max_points: maxPoints,
+      event_max_team_size: maxTeamSize,
+      event_is_open_event: isOpenEvent,
     } = row;
+
+    const {
+      event_contact_name: contactName,
+      event_contact_email: contactEmail,
+      event_contact_phone: contactPhone,
+    } = row;
+
+    const rules = await getRulesByEventId(id);
+
+    const requirements = await getRequirementsByEventId(id);
 
     const obj = {
       id,
       name,
-      to,
       description,
       img,
-      direction,
-      color,
-      text,
-      eventType,
+      rules,
+      requirements,
+      maxPoints,
+      maxTeamSize,
+      isOpenEvent,
+      contact: {
+        name: contactName,
+        email: contactEmail,
+        phone: contactPhone,
+      },
     };
+
     objs.push(obj);
   }
 
