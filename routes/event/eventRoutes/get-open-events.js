f74cb6cc83bff1ad99d8f365a3efde973c93f6ba
akashpunagin <akash.punagin@gmail.com>
2023-02-15 12:07:40 +0530

    event: get open events done

--- routes/event/eventRoutes/get-open-events.js
+++ routes/event/eventRoutes/get-open-events.js
@@ -1,34 +1,17 @@
-const pool = require("../../../db/pool");
 const { authorization } = require("../../../middleware/exportMiddlewares");
-const appConstants = require("../../../constants/appConstants");
+const { getLongEventDetails } = require("../../../scripts/getLongEventDetails");
 
 module.exports = (router) => {
   router.get("/get-open-events", [authorization], async (req, res) => {
     console.log("Route:", req.originalUrl);
 
-    const { eventMaster } = appConstants.SQL_TABLE;
-
     try {
-      const eventRes = await pool.query(`
-        SELECT * FROM ${eventMaster}
-        WHERE event_is_open_event = true
-      `);
-      let data = eventRes.rows;
-
-      data = data.map((event) => {
-        return {
-          eventId: event.event_id,
-          name: event.event_name,
-          description: event.event_description,
-          maxPoints: event.event_max_points,
-          maxTeamSize: event.event_max_team_size,
-          isOpenEvent: event.event_is_open_event,
-        };
-      });
+      let data = await getLongEventDetails();
+      data = data.filter((event) => event.isOpenEvent);
 
       return res.status(200).json(data);
     } catch (error) {
-      console.log("GET Event error", error);
+      console.log("GET Open Events error", error);
       return res.status(500).json("Server error");
     }
   });
