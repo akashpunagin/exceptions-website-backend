0b97b6505aaae2ac8b2c49ec278ebd6af4186020
akashpunagin <akash.punagin@gmail.com>
2023-02-16 17:01:20 +0530

    constants: get routes

--- routes/appConstants/appConstantsRouter.js
+++ routes/appConstants/appConstantsRouter.js
@@ -2,5 +2,9 @@ const router = require("express").Router();
 
 require("./appConstantsRoutes/get-max-gc-member-size")(router);
 require("./appConstantsRoutes/update-max-gc-member-size")(router);
+require("./appConstantsRoutes/get-event-fees-strike-force")(router);
+require("./appConstantsRoutes/get-event-fees-solvathon")(router);
+require("./appConstantsRoutes/get-event-fees-infinity-and-beyond")(router);
+require("./appConstantsRoutes/get-event-fees-all-group-events")(router);
 
 module.exports = router;
---
+++ routes/appConstants/appConstantsRoutes/get-event-fees-infinity-and-beyond.js
@@ -0,0 +1,31 @@
+const pool = require("../../../db/pool");
+const { authorization } = require("../../../middleware/exportMiddlewares");
+const appConstants = require("../../../constants/appConstants");
+
+module.exports = (router) => {
+  router.get(
+    "/get-event-fees-infinity-and-beyond",
+    [authorization],
+    async (req, res) => {
+      console.log("Route:", req.originalUrl);
+
+      const { appIntConstants } = appConstants.SQL_TABLE;
+      const label = "event_fees_infinity_and_beyond";
+
+      try {
+        const intConstantsRes = await pool.query(
+          `SELECT * FROM ${appIntConstants}
+        WHERE label = $1`,
+          [label]
+        );
+        const data = intConstantsRes.rows[0];
+        const maxGCMemberSize = data.value;
+
+        return res.status(200).json(maxGCMemberSize);
+      } catch (error) {
+        console.log("GET get-max-gc-member-size error", error);
+        return res.status(500).json("Server error");
+      }
+    }
+  );
+};
