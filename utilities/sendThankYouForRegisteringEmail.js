2934af9cbc75aeacdd13a7c52783770806191cf1
akashpunagin <akash.punagin@gmail.com>
2023-02-28 11:54:36 +0530

    thank you for registration email: add scribe documentation

--- utilities/sendThankYouForRegisteringEmail.js
+++ utilities/sendThankYouForRegisteringEmail.js
@@ -12,6 +12,8 @@ const transport = nodemailer.createTransport({
 
 const sendThankYouForRegisteringEmail = async (name, email) => {
   const telegramLink = "http://telegram.openinapp.co/Chanel";
+  const scribeLink =
+    "https://scribehow.com/shared/EXCEPTIONS-2023__mNwziZsfRtiTvJExqmhZEA";
   try {
     const temp = await transport.sendMail({
       from: process.env.NODEMAILDER_EMAIL,
@@ -109,6 +111,38 @@ const sendThankYouForRegisteringEmail = async (name, email) => {
           >
             Hey, ${name}. Your registration for Exceptions 2023 has been confirmed!
           </h3>
+
+          <br />
+
+          <h3 style="margin-top: 20px; box-sizing: border-box">
+            Further steps:
+          </h3>
+
+          <p style="font-style:bold; margin-top: 20px; box-sizing: border-box">
+          Step 1: Login to your account, and select team name and event type (Group events/Open events/Both)
+          <br />Step 2: Add your team-mates
+          <br />Step 3: Perform payment and upload the transaction screenshot and details
+          <br />
+          <br />(Step 2 and 3 can be performed simultaneously (ie You can also perform payment and add participants later)
+          <br />Step 4: Wait for payment verification from coordinators of Exceptions 2023 
+          <br />Step 5: Buckle up and prepare for take off! You're now a part of exclusive group of explorers!
+          <br />
+          <br />For detailed documentation with screenshots please have a look at the below documentation
+          </p>
+          <a
+            href=${scribeLink}
+            style="
+              border: 2px solid #dca54c;
+              padding: 8px 12px;
+              outline: none;
+              text-decoration: none;
+              color: #dca54c;
+              font-weight: bold;
+              margin: 0;
+              box-sizing: border-box;
+            "
+            >Detailed Registartion Process
+          </a>
           <h3
             style="margin-top: 20px; box-sizing: border-box"
           >
