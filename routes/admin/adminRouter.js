9ecefdfb4697bd0c978d223e3a0aeedfd3f443a6
akashpunagin <akash.punagin@gmail.com>
2023-03-01 16:27:04 +0530

    admin: get total team members of group events

--- routes/admin/adminRouter.js
+++ routes/admin/adminRouter.js
@@ -7,5 +7,6 @@ require("./adminRoutes/get-total-fees")(router);
 require("./adminRoutes/get-open-event-total-teams")(router);
 require("./adminRoutes/get-total-team-members")(router);
 require("./adminRoutes/get-group-event-total-teams")(router);
+require("./adminRoutes/get-total-team-members-of-group-events")(router);
 
 module.exports = router;
--- routes/admin/adminRoutes/get-total-team-members-of-group-events.js
+++ routes/admin/adminRoutes/get-total-team-members-of-group-events.js
@@ -1,16 +1,15 @@
-const pool = require("../../../db/pool");
 const appConstants = require("../../../constants/appConstants");
-const {
-  authorization,
-  authorizeAdmin,
-} = require("../../../middleware/exportMiddlewares");
+const { authorization } = require("../../../middleware/exportMiddlewares");
 const {
   getOnlyGroupEventsTeams,
 } = require("../../../dbUtils/event/dbEventUtils");
+const {
+  getTeamMembersByTeamId,
+} = require("../../../dbUtils/team_member_master/dbTeamMemberMasterUtils");
 
 module.exports = (router) => {
   router.get(
-    "/get-group-event-total-teams",
+    "/get-total-team-members-of-group-events",
     [authorization],
     async (req, res) => {
       console.log("Route:", req.originalUrl);
@@ -18,7 +17,13 @@ module.exports = (router) => {
       try {
         const teamIds = await getOnlyGroupEventsTeams();
 
-        return res.status(200).json(teamIds.length);
+        let totalTeamMembers = 0;
+        for (const teamId of teamIds) {
+          const numberOfTeamMembers = await getTeamMembersByTeamId(teamId);
+          totalTeamMembers += numberOfTeamMembers.length;
+        }
+
+        return res.status(200).json(totalTeamMembers);
       } catch (error) {
         console.log("GET All coordinator error", error);
         return res.status(500).json("Server error");
