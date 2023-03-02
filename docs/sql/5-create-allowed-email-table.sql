3ce511c1df67275121320e0168a91cb41e09a95f
akashpunagin <akash.punagin@gmail.com>
2023-03-02 17:42:08 +0530

    allowed emails: insert database

--- docs/sql/5-create-allowed-email-table.sql
+++ docs/sql/5-create-allowed-email-table.sql
@@ -3,31 +3,40 @@ CREATE TABLE allowed_emails_coordinators(
     email VARCHAR NOT NULL UNIQUE
 );
 
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('akashr.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('aksharamv.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('bharathgm.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('dhanyam.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('dilipsinghm.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('hanumantharh.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('mohammedanas.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('praveengoudab.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('pushpithal.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('rajashree.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('aiswaryacs.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('anjalisingh.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('manthankumars.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('abhishekjadhav.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('kajalpandey.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_coordinators(email)
+VALUES ('pk.puni872@gmail.com');
 
-INSERT INTO allowed_emails_coordinators(email)
-VALUES ('prajwalk.mca21@rvce.edu.in');
-INSERT INTO allowed_emails_coordinators(email)
-VALUES ('rakshithr.mca21@rvce.edu.in');
-INSERT INTO allowed_emails_coordinators(email)
-VALUES ('indrabusanam.mca21@rvce.edu.in');
-INSERT INTO allowed_emails_coordinators(email)
-VALUES ('aishwaryakk.mca21@rvce.edu.in');
-INSERT INTO allowed_emails_coordinators(email)
-VALUES ('Sumanthcr.mca21@rvce.edu.in');
-INSERT INTO allowed_emails_coordinators(email)
-VALUES ('Krupasa.mca21@rvce.edu.in');
-INSERT INTO allowed_emails_coordinators(email)
-VALUES ('shireeshakb.mca21@rvce.edu.in');
-INSERT INTO allowed_emails_coordinators(email)
-VALUES ('thejasp.mca21@rvce.edu.in');
-INSERT INTO allowed_emails_coordinators(email)
-VALUES ('bhaktiprabhu.mca21@rvce.edu.in');
-INSERT INTO allowed_emails_coordinators(email)
-VALUES ('sohangowdac.mca21@rvce.edu.in');
-INSERT INTO allowed_emails_coordinators(email)
-VALUES ('ranjithkj.mca21@rvce.edu.in');
-INSERT INTO allowed_emails_coordinators(email)
-VALUES ('winiljr.mca21@rvce.edu.in');
+
 
 CREATE TABLE allowed_emails_volunteers(
     id SERIAL PRIMARY KEY,
@@ -35,9 +44,27 @@ CREATE TABLE allowed_emails_volunteers(
 );
 
 INSERT INTO allowed_emails_volunteers(email)
-VALUES ('shashankt.mca21@rvce.edu.in');
+VALUES ('karthikk.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_volunteers(email)
+VALUES ('nisargak.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_volunteers(email)
+VALUES ('poornimar.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_volunteers(email)
+VALUES ('yashaswinis.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_volunteers(email)
+VALUES ('mohammedjunaid.mca22@rvce.edu.in');
+INSERT INTO allowed_emails_volunteers(email)
+VALUES ('sushantshegde.mca22@rvce.edu.in');
 INSERT INTO allowed_emails_volunteers(email)
 VALUES ('shimpyk.mca21@rvce.edu.in');
+INSERT INTO allowed_emails_volunteers(email)
+VALUES ('shaikrb.mca21@rvce.edu.in');
+INSERT INTO allowed_emails_volunteers(email)
+VALUES ('anjalipatel.mca21@rvce.edu.in');
+INSERT INTO allowed_emails_volunteers(email)
+VALUES ('harshithads.mca21@rvce.edu.in');
+INSERT INTO allowed_emails_volunteers(email)
+VALUES ('punithgsp.mca21@rvce.edu.in ');
 
 
 CREATE TABLE allowed_emails_admin(
@@ -45,5 +72,17 @@ CREATE TABLE allowed_emails_admin(
     email VARCHAR NOT NULL UNIQUE
 );
 
+INSERT INTO allowed_emails_admin(email)
+VALUES ('nithin.mca21@rvce.edu.in');
+INSERT INTO allowed_emails_admin(email)
+VALUES ('sanjanaga.mca21@rvce.edu.in');
+INSERT INTO allowed_emails_admin(email)
+VALUES ('ranjithkj.mca21@rvce.edu.in');
+INSERT INTO allowed_emails_admin(email)
+VALUES ('savitas.sheelavant@rvce.edu.in');
+INSERT INTO allowed_emails_admin(email)
+VALUES ('andhedharani@rvce.edu.in');
+INSERT INTO allowed_emails_admin(email)
+VALUES ('savitar@rvce.edu.in');
 INSERT INTO allowed_emails_admin(email)
 VALUES ('akash.punagin@gmail.com');
